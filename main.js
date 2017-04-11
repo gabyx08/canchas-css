9862213bc3917f717e853b81987b02e7ab48bda8
fbk-rena <gaby.ranarz@gmail.com>
2017-04-11 15:36:30 -0500

    cancha basquet

--- main.js
+++ main.js
@@ -20,20 +20,14 @@ function ocultarCanchas(){
 function mostrarCanchaSoccer(){
   ocultarCanchas();
   canchaFutbol.style.display="block";
-  canchaTenis.style.display="none";
-  canchaBasquet.style.display="none";
 }
 function mostrarCanchaBasquet(){
   ocultarCanchas();
-  canchaFutbol.style.display="none";
-  canchaTenis.style.display="none";
   canchaBasquet.style.display="block";
 }
 function mostrarCanchaTenis(){
   ocultarCanchas();
-  canchaFutbol.style.display="none";
   canchaTenis.style.display="block";
-  canchaBasquet.style.display="none";
 }
 
 ocultarCanchas();
